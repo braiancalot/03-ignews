1702208fa925e2e838382e649604576799b60d4e
braiancalot <calot665@hotmail.com>
2022-04-20 10:19:13 -0300

    font, dynamic titles and global style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
-import styles from '../styles/Home.module.scss'
+import Head from 'next/head'
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>Hello, <span>World</span></h1>
+    <>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+      <h1>Hello, World</h1>
+    </>
   );
 }
