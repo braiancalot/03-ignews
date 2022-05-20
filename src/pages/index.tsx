2e94133beb2902ac904e6b801a0ac89fc267c436
braiancalot <calot665@hotmail.com>
2022-05-19 21:05:46 -0300

    Stripe API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
-import Head from 'next/head'
+import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
+import { GetStaticProps } from 'next';
 
 import styles from './home.module.scss';
+import { stripe } from '../services/stripe';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product } : HomeProps) {
   return (
     <>
       <Head>
@@ -18,7 +27,7 @@ export default function Home() {
           </h1>
           
           <p>Get acess to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
 
           <SubscribeButton />
@@ -29,4 +38,23 @@ export default function Home() {
       </main>
     </>
   );
+}
+
+export const getStaticProps : GetStaticProps = async() => {
+  const price = await stripe.prices.retrieve('price_1L1IrgFBlmbF14GWf0SSayXI')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100)
+  }
+
+  return{
+    props: {
+      product
+    }, 
+    revalidate: 60 * 60 * 24, // 24 hours
+  };
 }
